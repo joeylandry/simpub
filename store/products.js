201bad26b44cb5cb61ef80a5fdcae473d2c6424d
joeylandry <152455833+joeylandry@users.noreply.github.com>
2025-04-25 14:56:11 -0400

    Add files via upload

---
+++ store/products.js
@@ -0,0 +1,44 @@
+const PRODUCTS = [
+    {
+      id: "face1",
+      name: "Face Tee 1",
+      image: "../face1.png",
+      price: 25,
+      tags: ["shirt", "psychedelic", "face"],
+      description: "Bold and weird. Trippy face tee on ultra-soft cotton."
+    },
+    {
+      id: "face2",
+      name: "Face Tee 2",
+      image: "../face2.png",
+      price: 26,
+      tags: ["shirt", "lineart", "psychedelic"],
+      description: "Scribble line art for cosmic chill."
+    },
+    {
+      id: "face3",
+      name: "Face Tee 3",
+      image: "../face3.png",
+      price: 27,
+      tags: ["psychedelic", "eye"],
+      description: "The third eye never blinks. Glow up time."
+    },
+    {
+        id: "face4",
+        name: "Face Tee 4",
+        image: "../face4.png",
+        price: 28,
+        tags: ["psychedelic", "face"],
+        description: "Bold and weird. Trippy face tee on ultra-soft cotton."
+      },
+      {
+        id: "face5",
+        name: "Face Tee 5",
+        image: "../face5.png",
+        price: 29,
+        tags: ["shirt", "lineart"],
+        description: "Scribble line art for cosmic chill."
+      },
+    // Add more products here...
+  ];
+  
